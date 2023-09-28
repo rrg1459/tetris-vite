e8ce7a21feb8035efbe24940d171c63507c50a79
rafaDev <rrg1459@hotmail.com>
2023-09-28 11:02:56 -0400

    Add function removeRows

--- main.js
+++ main.js
@@ -74,6 +74,7 @@ document.addEventListener('keydown', event => {
     if (checkCollision()) {
       piece.position.y--
       solidifyPiece()
+      removeRows()
     }
 })
 
@@ -95,6 +96,23 @@ function solidifyPiece () {
   })
   piece.position.x = 0
   piece.position.y = 0
+}
+
+function removeRows () {
+  const rowsToRemove = []
+
+  board.forEach((row, y) => {
+    // if (row.every(value => value > 0)) { is the same
+    if (row.every(value => value)) {
+      rowsToRemove.push(y)
+    }
+  })
+
+  rowsToRemove.forEach(y => {
+    board.splice(y, 1)
+    const newRow = Array(BOARD_WIDTH).fill(0)
+    board.unshift(newRow)
+  })
 }
 
 update()
